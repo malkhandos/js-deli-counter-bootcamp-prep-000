aa1c3b6aff44e3bb4cd22e47c98e0dc01629f128
Rommy Elyan <relyan@email.arizona.edu>
2018-04-05 19:18:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function nowServing(katzDeliLine) {
 	if (katzDeliLine.length === 0) {
 		return 'There is nobody waiting to be served!';
 	} else {
-		return `Currently serving ${katzDeliLine.shift()}.`;
+		return `Currently serving ${katzDeliLine.slice(0,1)}.`;
 	}
 }
